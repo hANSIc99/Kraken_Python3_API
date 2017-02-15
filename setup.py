39d1afd6962eae27df3b2a2bd4710e858fe1eb22
Stephan Avenwedde <s.avenwedde@gmail.com>
2017-02-15 19:46:16 +0100

    initial creation

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup, Extension
+import subprocess
+
+
+krakenprivatemodule = Extension('private',
+        extra_link_args=['-fPIC', '-D_GNU_SOURCE', '-D_POSIX_C_SOURCE=200112L' ],
+        include_dirs = ['.', '/usr/local/lib', '/usr/lib', '/usr/lib/x86_64-linux-gnu'],
+        libraries = [ 'kraken', 'crypto', 'm'],
+        library_dirs = ['.', '/usr/local/lib', '/usr/lib', '/usr/lib/x86_64-linux-gnu'],
+        sources = ['krakenprivatemodule.c']
+        )
+
+
+setup (name="private", version="1.0",
+      ext_modules=[krakenprivatemodule])
+
